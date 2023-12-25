e9f0c767f02a0b0d69b643d47182f12e05cf6611
Lucas Tempass <lucas.tempass@hotmail.com>
2023-12-25 18:24:37 -0300

    🚧 Creates GitmojiAction template

---
+++ src/main/kotlin/com/cerveira/lucas/gitmoji/actions/GitmojiAction.kt
@@ -0,0 +1,13 @@
+package com.cerveira.lucas.gitmoji.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GitmojiAction : AnAction() {
+
+    override fun actionPerformed(event: AnActionEvent) {
+        // TODO implement action body
+    }
+
+
+}
