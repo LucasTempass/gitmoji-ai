8c05231e4c7ca2f19b0e4474e0ff908472ff4dcc
Lucas Cerveira <lucas.tempass@hotmail.com>
2024-01-10 20:30:38 -0300

    ✨ Implement emoji suggestions

--- src/main/kotlin/com/cerveira/lucas/gitmoji/actions/GitmojiAction.kt
+++ src/main/kotlin/com/cerveira/lucas/gitmoji/actions/GitmojiAction.kt
@@ -1,15 +1,12 @@
 package com.cerveira.lucas.gitmoji.actions
 
 import com.cerveira.lucas.gitmoji.data.Gitmoji
-import com.cerveira.lucas.gitmoji.data.gitmojis
 import com.cerveira.lucas.gitmoji.notifications.sendNotification
 import com.cerveira.lucas.gitmoji.service.AIService
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.editor.colors.EditorFontType
-import com.intellij.openapi.progress.runBackgroundableTask
-import com.intellij.openapi.progress.runBlockingModal
 import com.intellij.openapi.progress.runModalTask
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopup
@@ -33,75 +30,80 @@ import javax.swing.ListSelectionModel
 
 class GitmojiAction : AnAction() {
 
-
     override fun actionPerformed(event: AnActionEvent) {
         val project = event.project ?: return
 
         val data = event.getData(VcsDataKeys.COMMIT_MESSAGE_CONTROL) ?: return
 
-        val commitMessage = data as CommitMessage
+        displaySuggestedEmojis(project, data as CommitMessage)
+    }
 
+    private fun displaySuggestedEmojis(
+        project: Project,
+        commitMessage: CommitMessage
+    ) {
         runModalTask(
-                "GENERATING EMOJI SUGGESTION", project) {
+            "Generating Emoji Suggestions", project
+        ) {
             val openAIService = AIService.instance
 
-            val sugested = runBlocking(Dispatchers.IO) {
+            val suggestedEmojis: List<Gitmoji>? = runBlocking(Dispatchers.IO) {
                 try {
                     val generatedEmojis = openAIService.generateSuggestedEmoji(
-                            "Suggested emoji for commit message: ${commitMessage.text}"
+                        commitMessage.text
                     )
-
-                    sendNotification("e.message.toString()", project)
 
                     return@runBlocking generatedEmojis;
                 } catch (e: Exception) {
-                    e.printStackTrace()
+                    sendNotification("There was an error generating the suggested emojis", project)
 
-                    sendNotification(e.message.toString(), project)
-
-                    return@runBlocking gitmojis;
+                    return@runBlocking null
                 }
             }
 
+            if (suggestedEmojis == null) {
+                return@runModalTask
+            }
+
             ApplicationManager.getApplication().invokeLater {
-                createEmojiSelector(commitMessage, project, sugested)
+                createEmojiSelector(commitMessage, project, suggestedEmojis)
             }
         }
-
     }
 
 
     private fun createEmojiSelector(
-            commitMessage: CommitMessage,
-            project: Project,
-            gitmojisOptions: List<Gitmoji>
+        commitMessage: CommitMessage,
+        project: Project,
+        gitmojisOptions: List<Gitmoji>
     ) {
         createPopup(commitMessage, gitmojisOptions, project).show(
-                RelativePoint(
-                        commitMessage.editorField, Point(0, JBUI.scale(16))
-                )
+            RelativePoint(
+                commitMessage.editorField, Point(0, JBUI.scale(16))
+            )
         )
     }
 
     private fun createPopup(
-            commitMessage: CommitMessage, gitmojis: List<Gitmoji>, project: Project
+        commitMessage: CommitMessage, gitmojis: List<Gitmoji>, project: Project
     ): JBPopup {
         val font = commitMessage.editorField.editor?.colorsScheme?.getFont(EditorFontType.PLAIN)
 
         val builder = JBPopupFactory.getInstance().createPopupChooserBuilder(gitmojis).setFont(font)
-                .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
-                .setVisibleRowCount(5)
-                .setItemChosenCallback {
-                    copyToClipboard(it.value)
-                }
-                .setRenderer(GitmojiColoredListCellRenderer())
-                .setNamerForFiltering { "${it.code} ${it.description}" }
-                .setAutoPackHeightOnFiltering(false)
+            .setSelectionMode(ListSelectionModel.SINGLE_SELECTION)
+            .setVisibleRowCount(5)
+            .setItemChosenCallback {
+                copyToClipboard(it.value)
+            }
+            .setRenderer(GitmojiColoredListCellRenderer())
+            .setNamerForFiltering { "${it.code} ${it.description}" }
+            .setAutoPackHeightOnFiltering(false)
 
         return builder.createPopup()
     }
 
     private fun copyToClipboard(str: String?) {
+        // TODO implement paste to commit message
         val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
         clipboard.setContents(StringSelection(str), null)
     }
@@ -110,7 +112,7 @@ class GitmojiAction : AnAction() {
 
 class GitmojiColoredListCellRenderer : ColoredListCellRenderer<Gitmoji>() {
     override fun customizeCellRenderer(
-            list: JList<out Gitmoji>, value: Gitmoji, index: Int, selected: Boolean, hasFocus: Boolean
+        list: JList<out Gitmoji>, value: Gitmoji, index: Int, selected: Boolean, hasFocus: Boolean
     ) {
         append(value.value, SimpleTextAttributes.REGULAR_ATTRIBUTES)
         append(" ", SimpleTextAttributes.GRAYED_SMALL_ATTRIBUTES)
